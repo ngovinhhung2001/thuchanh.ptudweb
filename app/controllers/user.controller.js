95fec14652f1e323ce7ac17723a898849e049a42
ngovinhhung2001 <vinhhung07103@gmail.com>
2023-02-22 10:38:34 +0700

    Cài đặt các handler truy xuất CSDL

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,15 +1,15 @@
 exports.showSignInPage = (req, res) => {
-    res.send({message: "show signIn page"});
+    res.send({ message: "show signIn page" });
 }
 
 exports.signIn = (req, res) => {
-    res.send({message: "signIn handler"});
+    res.send({ message: "signIn handler" });
 }
 
 exports.showSignUpPage = (req, res) => {
-    res.send({message: "show signUp page"});
+    res.send({ message: "show signUp page" });
 }
 
 exports.signUp = (req, res) => {
-    res.send({message: "signUp handler"});
+    res.send({ message: "signUp handler" });
 }
--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -7,6 +7,6 @@ router.route("/").get(contacts.findAll).post(contacts.create).delete(contacts.de
 
 router.route("/favorite").get(contacts.findAllFavorite);
 
-router.route("/:id").get(contacts.findOne).put(contacts.update).delete(contacts.findOne);
+router.route("/:id").get(contacts.findOne).put(contacts.update).delete(contacts.delete);
 
 module.exports = router;
